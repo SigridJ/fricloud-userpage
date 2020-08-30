c694e35eb27640db89a6985e5d6303c5afd93274
Sigrid Jensen <sigridjensen@fricloud.dk>
2020-08-30 19:42:25 +0200

    First draft of index.php and index.html

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+		<link rel="stylesheet" href="index.css"/>
+		<meta charset="UTF-8"/>
+		<title>Fricloud - Bruger</title>
+	</head>
+	<body>
+		<div class="ui container" style="margin: 5rem 8rem;">
+			<div class="ui basic segment" id="context">
+				<div class="ui left close dividing rail" style="width: 200px !important; top: 3rem;">
+					<div class="sticky" style="position: sticky; top: 6rem; z-index: inherit !important;">
+						<div class="ui vertical text menu" style="width: inherit;">
+							<div class="header item">Indstillinger</div>
+							<a class="item" href="#info">Brugeroplysninger</a>
+						</div>
+					</div>
+				</div>
+				<div>
+					<a class="anchor" id="info" style="top: -150px;"></a>
+					<h1 class="ui header">
+						Brugeroplysninger
+					</h1>
+					<div>
+						<h2 class="ui header">Skift adgangskode</h2>
+						<p>Du skal have adgang til din mailadresse du oprettede en bruger med for at skifte adgangskode.</p>
+						<button class="ui right floated disabled button">Skift Adgangskode</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
